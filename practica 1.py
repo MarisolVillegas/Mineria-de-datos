68d433681eebeacfb1ffb45fd39f954194e09263
MarisolVillegas <60374134+MarisolVillegas@users.noreply.github.com>
2022-09-20 14:54:50 -0500

    Practica 1

--- practica 1.py
+++ practica 1.py
@@ -1,8 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Marisol Villegas Rincón 1898149
 """
+import requests
+import io
+#from bs4 import BeautifulSoup
+import pandas as pd
+#from tabulate import tabulate
+#from typing import Tuple, List
+#import re
+#from datetime import datetime
+#import numpy as np
+
+
+def get_csv_from_url(url:str) -> pd.DataFrame:
+        s = requests.get(url).content
+        print("Lectura exitosa")
+        return pd.read_csv(io.StringIO(s.decode())) 
+        print("\nSe leyó de un archivo local")
+        
+       
+
+url = "https://raw.githubusercontent.com/MarisolVillegas/Mineria-de-datos/main/autos.csv"
+df = get_csv_from_url(url)
+print("\nDATAFRAME:\n",df)
 
  
